5677680dd41fbf743e1f9d2b55879eca9d28a6ec
Juan Cajamarca <juancajamarca@outlook.es>
2020-08-10 20:12:26 -0500

    Section 4. Create a Custom Image View

---
+++ swiftui-1/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  swiftui-1
+//
+//  Created by Juan David Cajamarca Acuña on 10/08/20.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 3))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
