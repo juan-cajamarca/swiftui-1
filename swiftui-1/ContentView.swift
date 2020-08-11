8e8b7e66a925941be7f6e51efa5ff7be90261116
Juan Cajamarca <juancajamarca@outlook.es>
2020-08-10 19:31:58 -0500

    Initial Commit

---
+++ swiftui-1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-1
+//
+//  Created by Juan David Cajamarca Acuña on 10/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
